d7127412688ccd1563c53c64731b01856bd5e445
Vy-UIT <19520359@gm.uit.edu.vn>
2022-11-05 09:22:02 +0700

    add

--- clothes-store/src/pages/about/contact/ContactPage.js
+++ clothes-store/src/pages/about/contact/ContactPage.js
@@ -5,10 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
-import LinhAva from '../../../assets/images/linh-ava.jpg'
-import KhangAva from '../../../assets/images/khang-ava.png'
-import MinhAva from '../../../assets/images/minh-ava.png'
-import VyAva from '../../../assets/images/vy-ava.jpg'
+import Ava from '../../../assets/images/full-ava.jpg'
+
 
 import './ContactPage.css'
 
@@ -30,22 +28,9 @@ function ContactPage() {
         </Modal.Header>
         <Modal.Body>
           <Container>
-            <Row>
-              <Col xs={6} md={6}>
-                <img className="contactImg" src={LinhAva} alt="VuTienLinh" />
-              </Col>
-              <Col xs={6} md={6}>
-                <img className="contactImg" src={KhangAva} alt="PhamHoangKhang" />
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={6} md={6}>
-                <img className="contactImg" src={MinhAva} alt="NguyenDangMinh" />
-              </Col>
-              <Col xs={6} md={6}>
-                <img className="contactImg" src={VyAva} alt="VoThiTuongVy" />
-              </Col>
-            </Row>
+            
+                <img className="contactImg" src={Ava} alt="avatar" />
+              
           </Container>
 
           <Form>
